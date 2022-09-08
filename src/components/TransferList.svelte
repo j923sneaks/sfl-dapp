160ddc0dff43b079c0266ff093ecf5ac0ee3d630
j923sneaks <j923sneaks@gmail.com>
2022-09-08 15:53:29 +0800

    init transfer list component

---
+++ src/components/TransferList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { FormData } from "../types";
+  import { ALL } from '../constants';
+
+  import TransferListItem from "./ui/TransferListItem.svelte";
+
+  export let list: FormData[] = [];
+
+</script>
+
+<div class="container">
+  {#each list as item}
+    <div>
+      {#each item.tokenIds as tokenId, index}
+        <TransferListItem item={ALL[tokenId]} amount={item.amounts[index].toNumber()} />
+      {/each}
+    </div>
+  {/each}
+</div>
